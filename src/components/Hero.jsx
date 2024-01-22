82fa78fe68394eeb04dae4d5e4dee9d4862074f5
Hetal-Ahir <ahirhetal2409@gmail.com>
2024-01-22 20:59:37 +0100

    Added img to Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-
+import teaImg from "../image/6.jpeg";
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div>
+      <img style={{ width: "100vw", height: "100vh" }} src={teaImg} alt="tea" />
+    </div>
+  );
 };
 
 export default Hero;

edc92e3b4d5ddcaee40ddba81bccb180b83d92a2
Hetal-Ahir <ahirhetal2409@gmail.com>
2024-01-23 10:03:58 +0100

    Add new hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,13 @@
 import teaImg from "../image/6.jpeg";
+// import logo from "../image/logo.png";
 const Hero = () => {
   return (
     <div>
       <img style={{ width: "100vw", height: "100vh" }} src={teaImg} alt="tea" />
+      {/* <div>
+        <p>Find your perfact tea</p>
+        <input type="text" />
+      </div> */}
     </div>
   );
 };

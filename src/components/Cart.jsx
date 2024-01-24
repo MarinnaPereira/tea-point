aab1fcddb264d44b38b70484f7e73b6952bca5bb
Hetal-Ahir <ahirhetal2409@gmail.com>
2024-01-24 09:18:03 +0100

    added cart.scss

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,24 +1,40 @@
+import { NavLink } from "react-router-dom";
 import { UseCartContext } from "../contexts/CartContext";
+import "../scss/Cart.scss";
 
 export const Cart = () => {
   const { productState } = UseCartContext();
   const cart = productState;
 
   return (
-    <>
-      {cart.length === 0 ? (
-        <div>
+    <div className="cart-container">
+      {!cart.length === 0 ? (
+        <div className="e-cart">
           <div>
-            <p>Your basket is empty.</p>
+            <p className="e-cart-massage">Your basket is empty.</p>
           </div>
-          <button>Start Shopping</button>
+          <NavLink to="/">
+            <button className="e-cart-button">Start Shopping</button>
+          </NavLink>
         </div>
       ) : (
-        <div>
-          <p>Estimated Total : </p>
-          <button>Checkout</button>
+        <div className="cart">
+          <div className="product-cart">
+            <h1>Your Cart</h1>
+            <div className="product">
+              <span>item1</span>
+              <span>$</span>
+            </div>
+          </div>
+          <div className="total-product">
+            <div>
+              <span className="cart-massage">Estimated Total </span>
+              <span className="cart-total">$</span>
+            </div>
+            <button className="cart-button">Checkout</button>
+          </div>
         </div>
       )}
-    </>
+    </div>
   );
 };

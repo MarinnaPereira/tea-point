1c987d7caa6abf2d3947a770aa3ccb427b6ce2b9
Yuko <yuko.gerheim@gmail.com>
2024-01-27 22:51:39 +0100

    Added assets, eslint line19, products, line29 in Nav.jsx, 3 x Route in App.jsx, modified Products.jsx & data.js

---
+++ src/components/products/Voucher.jsx
@@ -0,0 +1,53 @@
+import {products} from "../../data/products";
+import {useCartContext} from "../../hooks/useCartContext";
+
+const voucher = products.filter((product) => product.type === "voucher");
+
+const Voucher = () => {
+  const {addProduct} = useCartContext();
+  const handleAddToCart = (product) => {
+    addProduct(product);
+  };
+
+  return (
+    <section className="pb-5">
+      <h2 className="text-[34px] text-center mt-10 font-cinzel">
+        Gift Voucher
+      </h2>
+      <p className="text-center text-[#5f5f5f] italic px-4">
+        Gift the joy of exquisite teas to your loved one with our TEA POINT Gift
+        Voucher!
+      </p>
+      <p className="text-center text-[#5f5f5f] italic px-4">
+        Redeem this voucher for a variety of teas and teawares in-store or
+        online.
+      </p>
+      <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5 lg:px-32 md:px-24 px-12">
+        {voucher.map((product) => (
+          <div key={product.id} className="relative group">
+            <img
+              src={product.productImage}
+              alt={product.productName}
+              className="w-full h-auto transition-transform duration-300 transform group-hover:scale-105"
+            />
+            <div className="absolute inset-x-0 bottom-28 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
+              <button
+                className="bg-[#c0d1bd] w-full px-10 py-6 opacity-90"
+                onClick={() => {
+                  handleAddToCart(product);
+                }}
+              >
+                <p className="text-white text-[23px]">Add to Cart</p>
+              </button>
+            </div>
+            <h3 className="text-[25px] pt-5">{product.productName}</h3>
+            <p className="text-[#6f6f6f] italic">{product.description}</p>
+            <p className="text-[20px]">{product.price.toFixed(2)} Euro</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Voucher;
--- src/data/products.js
+++ src/data/products.js
@@ -13,12 +13,18 @@ import tea12 from "../assets/tea12.jpg";
 import cup1 from "../assets/cup1.jpg";
 import cup2 from "../assets/cup2.jpg";
 import cup3 from "../assets/cup3.jpg";
+import cup4 from "../assets/cup4.jpg";
+import cup5 from "../assets/cup5.jpg";
+import cup6 from "../assets/cup6.jpg";
 import set1 from "../assets/set1.jpg";
 import set2 from "../assets/set2.jpg";
 import set3 from "../assets/set3.jpg";
 import set4 from "../assets/set4.jpg";
 import set5 from "../assets/set5.jpg";
 import set6 from "../assets/set6.jpg";
+import voucher1 from "../assets/voucher1.jpg";
+import voucher2 from "../assets/voucher2.jpg";
+import voucher3 from "../assets/voucher3.jpg";
 
 export const products = [
   {
@@ -27,7 +33,8 @@ export const products = [
     productName: "Classic Earl Grey",
     price: 34.9,
     productImage: tea1,
-    description: "Black Tea",
+    description:
+      "Black Tea",
   },
   {
     id: "tl-2",
@@ -35,7 +42,8 @@ export const products = [
     productName: "Chamomile Mint",
     price: 28.9,
     productImage: tea2,
-    description: "Herbal Tea",
+    description:
+      "Herbal Tea",
   },
   {
     id: "tl-3",
@@ -43,7 +51,8 @@ export const products = [
     productName: "Green Jasmine",
     price: 32.9,
     productImage: tea3,
-    description: "Green Tea",
+    description:
+      "Green Tea",
   },
   {
     id: "tl-4",
@@ -51,7 +60,8 @@ export const products = [
     productName: "Masala Chai",
     price: 34.9,
     productImage: tea4,
-    description: "Spiced Black Tea",
+    description:
+      "Spiced Black Tea",
   },
   {
     id: "tl-5",
@@ -59,7 +69,8 @@ export const products = [
     productName: "Hibiscus Berry",
     price: 29.9,
     productImage: tea5,
-    description: "Herbal Tea",
+    description:
+      "Herbal Tea",
   },
   {
     id: "tl-6",
@@ -67,7 +78,8 @@ export const products = [
     productName: "Oolong Orchid",
     price: 36.9,
     productImage: tea6,
-    description: "Oolong Tea",
+    description:
+      "Oolong Tea",
   },
   {
     id: "tl-7",
@@ -75,7 +87,8 @@ export const products = [
     productName: "White Peach Blossom",
     price: 28.9,
     productImage: tea7,
-    description: "White Tea",
+    description:
+      "White Tea",
   },
   {
     id: "tl-8",
@@ -83,7 +96,8 @@ export const products = [
     productName: "Peppermint Twist",
     price: 34.9,
     productImage: tea8,
-    description: "Herbal Tea",
+    description:
+      "Herbal Tea",
   },
   {
     id: "tl-9",
@@ -91,7 +105,8 @@ export const products = [
     productName: "Lemon Ginger Detox",
     price: 32.9,
     productImage: tea9,
-    description: "Herbal Tea",
+    description:
+      "Herbal Tea",
   },
   {
     id: "tl-10",
@@ -99,7 +114,8 @@ export const products = [
     productName: "Rooibos Vanilla Caramel",
     price: 28.9,
     productImage: tea10,
-    description: "Rooibos Tea",
+    description:
+      "Rooibos Tea",
   },
   {
     id: "tl-11",
@@ -107,7 +123,8 @@ export const products = [
     productName: "Golden Turmeric Blend",
     price: 31.9,
     productImage: tea11,
-    description: "Herbal Tea",
+    description:
+      "Herbal Tea",
   },
   {
     id: "tl-12",
@@ -115,7 +132,8 @@ export const products = [
     productName: "Blueberry Lavender Dream",
     price: 33.9,
     productImage: tea12,
-    description: "Herbal Tea",
+    description:
+      "Herbal Tea",
   },
   {
     id: "tc-1",
@@ -128,10 +146,10 @@ export const products = [
   {
     id: "tc-2",
     type: "teaware",
-    productName: "Tea Cup - Elegant",
+    productName: "Tea Cup - Romance",
     price: 51.9,
     productImage: cup2,
-    description: "Indulge in sophistication",
+    description: "Taste of sweet moments",
   },
   {
     id: "tc-3",
@@ -139,7 +157,31 @@ export const products = [
     productName: "Tea Cup - Joy",
     price: 38.9,
     productImage: cup3,
-    description: "Savor the sweetness of happiness",
+    description: "delight of happiness",
+  },
+  {
+    id: "tc-4",
+    type: "teaware",
+    productName: "Tea Cup - Reflection",
+    price: 41.9,
+    productImage: cup4,
+    description: "Timeless allure",
+  },
+  {
+    id: "tc-5",
+    type: "teaware",
+    productName: "Tea Cup - Kyoto",
+    price: 51.9,
+    productImage: cup5,
+    description: "Journey back in time",
+  },
+  {
+    id: "tc-6",
+    type: "teaware",
+    productName: "Tea Cup - Elegance",
+    price: 50.9,
+    productImage: cup6,
+    description: "Indulge in sophistication",
   },
   {
     id: "ts-1",
@@ -182,11 +224,35 @@ export const products = [
     description: "Fascinating journey",
   },
   {
-    id: "ts-6",
+    id: "vo-6",
     type: "teaware",
     productName: "Tea Set - Oasis",
     price: 230.9,
     productImage: set6,
     description: "Serene moments of life",
+  },
+  {
+    id: "vo-1",
+    type: "voucher",
+    productName: "Voucher 50",
+    price: 50.0,
+    productImage: voucher1,
+    description: "Sip and save on your favorite teas",
+  },
+  {
+    id: "vo-2",
+    type: "voucher",
+    productName: "Voucher 100",
+    price: 100.0,
+    productImage: voucher2,
+    description: "Elevate your tea moments with us",
+  },
+  {
+    id: "vo-3",
+    type: "voucher",
+    productName: "Voucher 200",
+    price: 200.0,
+    productImage: voucher3,
+    description: "Explore premium blends on us",
   },
 ];

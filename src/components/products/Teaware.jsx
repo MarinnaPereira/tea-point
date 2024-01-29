01b6205402929812b0bb1dae8121d68fdf30a55b
Yuko <yuko.gerheim@gmail.com>
2024-01-29 22:30:12 +0100

    Changed tea-leaves.png to transparent, modified Oasis id in products.js and responsive design in Products.jsx & 3 * jsx in product

--- src/components/products/Teaware.jsx
+++ src/components/products/Teaware.jsx
@@ -25,32 +25,36 @@ export const Teaware = () => {
         <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5 lg:px-32 md:px-24 px-12">
           {teaWare.map((product) => (
             <div key={product.id} className="relative group">
-              <img
-                src={product.productImage}
-                alt={product.productName}
-                className="w-full h-auto transition-transform duration-300 transform lg:group-hover:scale-105"
-              />
-              <div className="absolute inset-x-0 bottom-28 flex items-center justify-center lg:opacity-0 lg:group-hover:opacity-100 transition-opacity">
-                <button
-                  className="bg-[#c0d1bd] w-full m-2 lg:m-0 px-10 py-6 opacity-90"
-                  onClick={() => {
-                    handleAddToCart(product);
-                  }}
-                >
-                  <p className="text-white text-[23px]">Add to Cart</p>
-                </button>
+              <div className="relative">
+                <img
+                  src={product.productImage}
+                  alt={product.productName}
+                  className="w-full h-auto lg:transition-transform lg:duration-300 lg:transform lg:group-hover:scale-105"
+                />
+                <div className="absolute inset-x-0 bottom-0 flex items-center justify-center lg:opacity-0 lg:group-hover:opacity-100 transition-opacity">
+                  <button
+                    className="bg-[#c0d1bd] w-full m-2 lg:m-0 px-10 py-6 opacity-90"
+                    onClick={() => {
+                      handleAddToCart(product);
+                    }}
+                  >
+                    <p className="text-white text-[23px]">Add to Cart</p>
+                  </button>
+                </div>
               </div>
-              <h3 className="text-[25px] pt-5">{product.productName}</h3>
-              <p className="text-[#6f6f6f] italic">{product.description}</p>
-              {product.id.startsWith("tc") ? (
-                <p className="text-[20px]">
-                  {product.price.toFixed(2)} Euro / 1 pc
-                </p>
-              ) : product.id.startsWith("ts") ? (
-                <p className="text-[20px]">
-                  {product.price.toFixed(2)} Euro / 1 pot & 2 cups
-                </p>
-              ) : null}
+              <div className="flex flex-col">
+                <h3 className="text-[25px] pt-5">{product.productName}</h3>
+                <p className="text-[#6f6f6f] italic">{product.description}</p>
+                {product.id.startsWith("tc") ? (
+                  <p className="text-[20px]">
+                    {product.price.toFixed(2)} Euro / 1 pc
+                  </p>
+                ) : product.id.startsWith("ts") ? (
+                  <p className="text-[20px]">
+                    {product.price.toFixed(2)} Euro / 1 pot & 2 cups
+                  </p>
+                ) : null}
+              </div>
             </div>
           ))}
         </div>
